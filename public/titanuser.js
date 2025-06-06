b40f3624fced21d7459a35c81c36d225257d2559
Heaven0016 <ryan.aqwww@hotmail.com>
2025-06-06 10:49:27 -0300

    Update and rename titan.user.js to titanuser.js

--- public/titanuser.js
+++ public/titanuser.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cadeira de Rodas pro Titan
 // @namespace    http://tampermonkey.net/
-// @version      70
+// @version      72.0
 // @description  Script facilita a vida do programador nesse terminal.
 // @author       Heaven
 // @match        */albatros/admin.php*seccion=titan_trabajos_ordenes*
